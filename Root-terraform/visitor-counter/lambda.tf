9315aea69beffc91856b13645bdf34223ce3ef64
Paolo Dominic Zafra <12345678+Z4aF@users.noreply.github.com>
2026-03-23 19:43:31 +0800

    Finalize CI and backend deploy setup

--- Root-terraform/visitor-counter/lambda.tf
+++ Root-terraform/visitor-counter/lambda.tf
@@ -1,6 +1,6 @@
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/visitor-counter/lambdas"
+  source_dir  = "${path.module}/lambdas"
   output_path = "${path.module}/lambda.zip"
 }
 
